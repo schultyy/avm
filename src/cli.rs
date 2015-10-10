55b8f753aa9467096fbfd3c3e7b6795c73a88c99
Jan Schulte <hello@unexpected-co.de>
2015-10-10 22:49:47 +0200

    ship self written bare minimum argument parser

--- src/cli.rs
+++ src/cli.rs
@@ -1,43 +1,17 @@
-extern crate getopts;
-use self::getopts::Options;
-use std::env;
-
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} install <version>", program);
-    print!("{}", opts.usage(&brief));
+pub fn help() {
+    println!("Usage:");
+    println!("avm install <version>");
 }
 
-fn install_version(version_str: &String) {
-    println!("Installing node {}", version_str);
-}
-
-pub fn process_arguments(program: String, args: Vec<String>) {
-    let mut opts = Options::new();
-    opts.optopt("", "install", "", "VERSION");
-    opts.optflag("h", "help", "print this help menu");
-
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
-    };
-    if matches.opt_present("h") {
-        print_usage(&program, opts);
-        return;
+pub fn process_arguments(args: &Vec<String>) -> Option<String> {
+    if args.len() < 2 {
+        return None
     }
-
-    println!("{:?}", matches.free);
-
-    if !matches.opt_present("install") {
-        print_usage(&program, opts);
-        return;
+    let command = args[0].clone();
+    if command == "install" {
+        Some(args[1].clone())
     }
-
-    let input = if matches.free.len() > 1 {
-        matches.free[1].clone()
-    } else {
-        print_usage(&program, opts);
-        return;
-    };
-
-    install_version(&input);
+    else {
+        None
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 mod cli;
+use std::process;
 use std::env;
 
 fn main() {
     println!("avm");
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-    cli::process_arguments(program, args);
+    let args: Vec<String> = env::args()
+        .skip(1)
+        .collect();
+    let version = match cli::process_arguments(&args) {
+        Some(version) => version,
+        None => {
+            cli::help();
+            std::process::exit(1)
+        }
+    };
 }
