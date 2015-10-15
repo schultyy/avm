46eb707f6407773337e00bff0429ee5700c53a0b
Jan Schulte <hello@unexpected-co.de>
2015-10-15 21:37:09 +0200

    Improve output of ls

--- src/ls.rs
+++ src/ls.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use setup;
 use std::fs;
 
@@ -8,13 +9,22 @@ fn is_directory(path: &String) -> bool {
     }
 }
 
+fn directory_name(full_path: &String) -> String {
+    let components = Path::new(full_path).components();
+    components.last().unwrap()
+        .as_os_str()
+        .to_str()
+        .unwrap()
+        .into()
+}
+
 pub fn ls_versions() -> Vec<String> {
     let home = setup::avm_directory();
     let mut paths = Vec::new();
     for path in fs::read_dir(home).unwrap() {
         let path_str = path.unwrap().path().display().to_string();
         if is_directory(&path_str) {
-            paths.push(path_str);
+            paths.push(directory_name(&path_str));
         }
     }
     paths
