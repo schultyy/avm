619585b20c62436ea2eaa475b47817069a0afb5f
Jan Schulte <hello@unexpected-co.de>
2015-11-03 22:46:55 +0100

    add test for upgrading npm
    this test ensures that symlinks are set correctly and the new npm
    version is accessible

---
+++ tests/upgrade_npm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cargo run install 0.10.36
+cargo run use 0.10.36
+old_npm_version=$(npm -v)
+npm install -g npm
+new_npm_version=$(npm -v)
+
+if [ new_npm_version == old_npm_version ]
+then
+  echo "fatal: did not successfully upgrade npm"
+  rm -rf ~/.avm
+  exit 1
+else
+  echo "Upgraded npm successfully"
+  rm -rf ~/.avm
+fi
